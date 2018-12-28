5693dfb79eb463950e72561e468bb194cf76bb23
Annapurna Avvaru <anna.avvaru@gmail.com>
2018-12-28 15:13:44 -0500

    reset soft

--- login_avvaru.php
+++
@@ -1,6 +0,0 @@
-<?php // login.php   make sure to place in personal folder
-$db_hostname = 'localhost';
-$db_database = 'aarutge1_mi550';
-$db_username = 'aarutge1'; // 'yourFTPusername'
-$db_password = 'Chirala9763'; // 'yourFTPpassword'
-?>
